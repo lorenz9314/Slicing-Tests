52bd7bef89b0f795f38bf2dfc567efb60b868f9d
Lorenz Hüther <lorenz1@uni-bremen.de>
2022-04-06 16:57:58 +0200

    Update docker folder and add SVF image.

--- Docker/SVF/run.sh
+++ Docker/SVF/run.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env sh
 
-docker build -t svf:latest .
-docker run --rm -ti --name svf \
-	-v "${PWD}/../../examples:/examples" \
-	-v "${PWD}/../../files:/files" \
+docker build --build-arg UID=$(id -u) --build-arg GID=$(id -g) \
+  	-f Dockerfile -t svf:latest .
+docker run -ti --name svf \
+	-v "${PWD}/../../examples:/examples:Z" \
+	-v "${PWD}/../../files:/files:Z" \
 	svf:latest
